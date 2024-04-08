8e19f1283a29e943e4a85baf4f5663e8dacf05d3
Larissa-Santos16 <larisan970@gmail.com>
2024-04-07 22:44:34 -0300

    Criacao da Home Page parte 1

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container-fluid text-center">
+    <div class="row">  
+      <div class="col-6"></div>
+      <div class="col-6"><button type="button" style="background-color: #6EBAF9; border:none" class="btn btn-primary rounded-3">Visualizar Escolas</button>
+      </div>
+      <div class="col-6"></div>
+    </div>
+    <div class="d-block p-3">
+      <div><button type="button" style="background-color: #6EBAF9; border:none" class="btn btn-primary rounded-3">Configurar E-mail</button></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeComponent",
+}
+</script>
+
+<style scoped></style>
